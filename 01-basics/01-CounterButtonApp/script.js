6207bf08f3431fec4b31425c9784145f553d450e
Maiia <maiiapo.com@gmail.com>
2023-03-25 12:24:43 +0500

    Реализация счетчика

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,13 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const App = {
+  name: 'App',
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+
+const app = createApp(App);
+const vm = app.mount('#app');
