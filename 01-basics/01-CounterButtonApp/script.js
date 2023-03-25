c34f0ca61596438d93964725092f6760aa12a30e
Maiia <maiiapo.com@gmail.com>
2023-03-25 12:32:49 +0500

    Реализация счетчика: добавление функции defineComponent

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,13 +1,13 @@
-import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-const App = {
+const App = defineComponent({
   name: 'App',
   data() {
     return {
       count: 0,
     };
   },
-};
+});
 
 const app = createApp(App);
 const vm = app.mount('#app');
