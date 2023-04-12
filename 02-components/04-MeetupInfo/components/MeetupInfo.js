bd0e46c02909d170ecd26141115aad5580a2f1a1
Maiia <maiiapo.com@gmail.com>
2023-04-12 22:56:21 +0500

    Краткая информация о митапе

--- 02-components/04-MeetupInfo/components/MeetupInfo.js
+++ 02-components/04-MeetupInfo/components/MeetupInfo.js
@@ -2,20 +2,51 @@ import { defineComponent } from '../vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupInfo',
+  props: {
+    organizer: {
+      type: String,
+      required: true,
+    },
+    place: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Number,
+      required: true,
+    }
+  },
+
+  methods: {
+    formatDate() {
+      return new Date(this.date).toLocaleDateString(navigator.language, {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+      })
+    },
+    formatDateForAttribute() {
+      return new Date(this.date).toLocaleDateString('fr-CA', {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+      })
+    },
+  },
 
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="formatDateForAttribute()">{{ formatDate() }}</time>
       </li>
     </ul>`,
 });
