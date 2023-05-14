46574d3580f37db697e018b68a79093f2d09a793
Maiia <maiiapo.com@gmail.com>
2023-05-14 23:16:43 +0500

    Реализация компонента UiCheckbox

--- 06-wrappers/04-UiCheckbox/components/UiCheckbox.vue
+++ 06-wrappers/04-UiCheckbox/components/UiCheckbox.vue
@@ -1,14 +1,36 @@
 <template>
   <label class="checkbox">
-    <input type="checkbox" class="checkbox__input" />
+    <input
+      type="checkbox"
+      class="checkbox__input"
+      v-bind="$attrs"
+      v-model="modelValueProxy"
+    />
     <span class="checkbox__box"></span>
-    Label Text
+    <slot />
   </label>
 </template>
 
 <script>
 export default {
   name: 'UiCheckbox',
+  inheritAttrs: false,
+  props: {
+    modelValue: {
+      type: [Boolean, Array, Set]
+    }
+  },
+  emits: ['update:modelValue'],
+  computed: {
+    modelValueProxy: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value)
+      }
+    }
+  },
 };
 </script>
 
