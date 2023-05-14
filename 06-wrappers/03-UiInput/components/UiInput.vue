512902fafed03928da64938102e2c75d3ef2b8d5
Maiia <maiiapo.com@gmail.com>
2023-05-14 18:09:38 +0500

    Разработка универсального компонента ввода (UiInput)

--- 06-wrappers/03-UiInput/components/UiInput.vue
+++ 06-wrappers/03-UiInput/components/UiInput.vue
@@ -1,20 +1,70 @@
 <template>
-  <div class="input-group input-group_icon input-group_icon-left input-group_icon-right">
-    <div class="input-group__icon">
-      <img class="icon" alt="icon" />
+  <div
+    class="input-group"
+    :class="{
+      'input-group_icon' : !!$slots['left-icon'] || !!$slots['right-icon'],
+      'input-group_icon-left' : !!$slots['left-icon'],
+      'input-group_icon-right' : !!$slots['right-icon']
+    }"
+  >
+
+    <div v-if="!!$slots['left-icon']"  class="input-group__icon">
+      <slot name="left-icon"></slot>
     </div>
 
-    <input ref="input" class="form-control form-control_rounded form-control_sm" />
-
-    <div class="input-group__icon">
-      <img class="icon" alt="icon" />
+    <component :is="tag"
+      ref="input"
+      v-bind="$attrs"
+      :value="modelValue"
+      class="form-control"
+      :class="{'form-control_sm': small, 'form-control_rounded': rounded}"
+      @focus="focus"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+    <div v-if="!!$slots['right-icon']"  class="input-group__icon">
+      <slot name="right-icon"></slot>
     </div>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'UiInput',
+  inheritAttrs: false,
+  emits: ['update:modelValue', 'focus'],
+  props: {
+    modelValue: {
+      type: String
+    },
+    small: {
+      type: Boolean,
+    },
+    rounded: {
+      type: Boolean,
+    },
+    multiline : {
+      type: Boolean,
+    }
+  },
+  computed: {
+    modelValueProxy: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value)
+      }
+    },
+    tag() {
+      return this.multiline ? 'textarea' : 'input';
+    }
+  },
+  methods: {
+    focus() {
+      this.$refs.input.focus();
+    }
+  }
 };
 </script>
 
